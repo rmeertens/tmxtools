b89681e7f2ab9247351911e743c08d9cb10b8a8c
Roland Meertens <rolandmeertens@gmail.com>
2017-04-03 15:23:49 +0200

    +make sure input and output are correct

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='tmxtools',
-      version='0.0.2',
+      version='0.0.3',
       description='Tools for translation memory files (TMX)',
       author='Roland Meertens',
       author_email='rolandmeertens@gmail.com',
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,20 @@
 import unittest
 from tmxtools import validate_translation_memory
-
+from tmxtools import split_translation_memory
 class allTests(unittest.TestCase):
     def testReading(self):
         self.assertTrue(validate_translation_memory.is_tmx_valid("tests/testfiles/test1.tmx"))
+
+    def testReadsCorrect(self):
+        inputsread = []
+        outputsread = []
+        for inputsentence, outputsentence in split_translation_memory.get_input_output("tests/testfiles/test1.tmx"):
+            inputsread.append(inputsentence)
+            outputsread.append(outputsentence)
+        self.assertEqual(len(inputsread),1)
+        self.assertEqual(len(outputsread), 1)
+        self.assertEqual(inputsread[0], "Test this input")
+        self.assertEqual(outputsread[0], "Test deze invoer")
 
 
 def main():
