2d9edc7d30522a94238d233fb38207825c653b33
Roland Meertens <rolandmeertens@gmail.com>
2017-04-03 14:48:27 +0200

    +travis config, +removed generic tests that had nothing to do with our code

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,28 +1,9 @@
 import unittest
-# import the package
-import tmxtools
 from tmxtools import validate_translation_memory
 
-# Here's our "unit".
-def IsOdd(n):
-    return n % 2 == 1
-
-# Here's our "unit tests".
-class IsOddTests(unittest.TestCase):
-
-    def testOne(self):
-        self.failUnless(IsOdd(1))
-
-    def testTwo(self):
-        self.failIf(IsOdd(2))
-    def testThree(self):
-        self.failIf(IsOdd(4))
-    def testFour(self):
-        self.failIf(IsOdd(6))
-
-class  allTests(unittest.TestCase):
+class allTests(unittest.TestCase):
     def testReading(self):
-        self.assertTrue(validate_translation_memory.is_tmx_valid("test1.tmx"))
+        self.assertTrue(validate_translation_memory.is_tmx_valid("tests/testfiles/test1.tmx"))
 
 
 def main():
